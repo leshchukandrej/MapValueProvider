48a3c71caf51b20cd8f567f9340f70871c363ffa
andrej_leshchuk <andrej.leshchuk@gmail.com>
2019-05-28 15:37:14 +0300

    no message

--- src/aura/MapValueProvider/MapValueProviderHelper.js
+++ src/aura/MapValueProvider/MapValueProviderHelper.js
@@ -15,20 +15,32 @@
     },
 
     getBodyCmps: function (component) {
-        if (this.isValueNotUpdated(component)) {
+        // if (this.isValueNotUpdated(component)) {
+        //     return;
+        // }
+
+        if (!component.get('v.map') || !component.get('v.key')) {
             return;
         }
 
-        return this.generateTemplateElements(component);
+        let previousKey = component.get('v.previousKey') //todo
+
+        if (previousKey === component.get('v.key')) {
+            return;
+        }
+
+        component.set('v.previousKey', component.get('v.key'))
+
+        return this.generateTemplateElements(component, previousKey);
     },
 
-    generateTemplateElements: function (component) {
+    generateTemplateElements: function (component, previousKey) {
         let helper = this;
         let newCmps = [];
 
         helper.forEach(component.get('v.body'), cmpDefRef => {
             let valueProvider = helper.getValueProvider(cmpDefRef);
-            helper.setChildComponentDefRef(component, cmpDefRef, valueProvider, component.get('v.var'));
+            helper.setChildComponentDefRef(component, cmpDefRef, valueProvider, component.get('v.var'), previousKey);
             let newCmp = helper.genNewCmp(cmpDefRef);
             newCmps.push(newCmp);
         })
@@ -36,9 +48,10 @@
         return newCmps;
     },
 
-    setChildComponentDefRef: function (component, componentDefRef, valueProvider, varName) {
+    setChildComponentDefRef: function (component, componentDefRef, valueProvider, varName, previousKey) {
         let helper = this;
         let values = componentDefRef.attributes.values;
+        let key = component.get('v.key');
 
         for (let tempKey in values) {
 
@@ -47,12 +60,21 @@
             let path = helper.getConvertedPath(tempPath);
             let isVarNameInPath = helper.isVarNameInPath(path, varName);
 
-            if (tempKey === 'body' || !tempPath || (Array.isArray(tempPath) && !isVarNameInPath)) {
+            if (tempKey !== 'body' && !tempPath && typeof(tempValue.value) === 'object') {
+                let ref = tempValue.value.toString();
+                path = ref.substring(ref.indexOf('{!')+2, ref.indexOf('}'));
+                if (path == 'v.map.' + previousKey) {
+                    component.set(path.replace(varName, 'v.map[' + key + ']'), component.getReference(path.replace(varName, 'v.map[' + key + ']')))
+                }
+                continue
+            }
+
+            if (tempKey === 'body' || !path || (Array.isArray(tempPath) && !isVarNameInPath)) {
                 continue;
             }
 
             if (isVarNameInPath) {
-                tempValue.value = component.getReference(path.replace(varName, 'v.returnValue'));
+                tempValue.value = component.getReference(path.replace(varName, 'v.map[' + key + ']'));
             } else if (valueProvider) {
                 tempValue.value = valueProvider.getReference(path);
             }
@@ -60,7 +82,7 @@
 
         if (values.body) {
             helper.forEach(values.body.value, cmpDefRef => {
-                helper.setChildComponentDefRef(component, cmpDefRef, valueProvider, varName);
+                helper.setChildComponentDefRef(component, cmpDefRef, valueProvider, varName, previousKey);
             })
         }
     },
@@ -92,7 +114,7 @@
             return true;
         }
 
-        component.set('v.returnValue', value);
+        // component.set('v.returnValue', value);
     },
 
     isValuesAreSame: function (value, oldValue) {
@@ -116,12 +138,13 @@
         let map = component.get('v.map');
         let key = component.get('v.key');
 
-        return this.getValue(map, key);
+        return this.getValue(map, key, component);
     },
 
-    getValue: function (map, key) {
+    getValue: function (map, key, component) {
         if (map && key) {
             return (map instanceof Map) ? map.get(key) : map[key];
+            // return component.getReference('v.map[' + key + ']');
         }
     },
 
